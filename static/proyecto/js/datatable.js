15d214d822a124e0c8cee6c0a636e301474d8065
Juan Capdevila <juancapde99@gmail.com>
2023-04-28 19:32:04 -0300

    *Acompaña commit anterior

---
+++ static/proyecto/js/datatable.js
@@ -0,0 +1,28 @@
+datatable_es = {
+    "sProcessing":    "Procesando...",
+    "sLengthMenu":    "",
+    "sZeroRecords":   "No se encontraron resultados",
+    "sEmptyTable":    "No se encontraron resultados",
+    "sInfo":         "_MAX_ Registros",
+    "sInfoEmpty":     "0 Registros",
+    "sInfoFiltered":  "(filtrando de _MAX_ registros)",
+    "sSearch":        "Buscar:",
+    "sLoadingRecords": "Cargando...",
+    "paginate": {
+        "first":      "Prim.",
+        "last":       "Últ.",
+        "next":       ">",
+        "previous":   "<"
+    },
+    "sAria": {
+        "SortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "SortDescending": ": Activar para ordenar la columna de manera descendente"
+    },
+    select: {
+        rows: {
+          _: "; %d re. seleccionados",
+          0: "",
+          1: "; 1 registro seleccionado"
+      }
+    }
+}
